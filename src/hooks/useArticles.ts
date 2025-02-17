68790f4ab9c5854d21e0e2c6f950ae3f01a1a3f7
Mariam Wagdy <mariam.wagdy92@gmail.com>
2025-02-18 01:18:25 +0200

    Enhancement and self-review
    - Article viewer enhancement responsive design
    - Implementing load more to Articles list

--- src/hooks/useArticles.ts
+++ src/hooks/useArticles.ts
@@ -1,15 +1,23 @@
-import { useState } from "react";
-import { getArticles } from "@api";
+import {useState, useEffect} from "react";
+import {getArticles} from "@api";
 
 const useArticles = () => {
     const [articles, setArticles] = useState<any[]>([]);
     const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
+    const [filters, setFilters] = useState({});
 
-    const fetchArticles = async (filters: any) => {
+    const fetchArticles = async (newFilters = filters, reset = false, newPage = page) => {
         setLoading(true);
         try {
-            const result = await getArticles(filters);
-            setArticles(result.data);
+            const result = await getArticles({...newFilters, page: newPage});
+            if (reset || newPage === 1) {
+                setArticles(result.data);
+            } else {
+                setArticles((prev) => [...prev, ...result.data]);
+            }
+            setHasMore(result.next_page_url !== null);
         } catch (error) {
             console.error("Error fetching articles:", error);
         } finally {
@@ -17,7 +25,24 @@ const useArticles = () => {
         }
     };
 
-    return { articles, loading, fetchArticles };
+    // resetting page number on filter changes
+    const handleFilterChange = (newFilters: any) => {
+        setFilters(newFilters);
+        fetchArticles(newFilters, true, 1);
+    };
+
+    // apply load more
+    useEffect(() => {
+        fetchArticles(filters, false);
+    }, [page]);
+
+    const loadMore = () => {
+        if (hasMore && !loading) {
+            setPage((prevPage) => prevPage + 1);
+        }
+    };
+
+    return {articles, loading, fetchArticles: handleFilterChange, loadMore, hasMore};
 };
 
 export default useArticles;
